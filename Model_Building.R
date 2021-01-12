f2af0b8f609da59e163b83e138eba60a300d603e
JyothiTom <59750681+JyothiTom@users.noreply.github.com>
2021-01-13 01:34:58 +0530

    Improved the model
    Improved the model and added comments

--- Model_Building.R
+++ Model_Building.R
@@ -1,97 +1,161 @@
 """
 	@script-author: Jyothi Tom, Shekhina Neha, Gokul S, Aishwarya Lakshmi, Srija S, Robin Wilson
 	@script-description: R Code to clean the data and train the Logistic Regression Model  
-  @script-details: Written in RStudio
+  	@script-details: Written in RStudio
 """
-
 library(DescTools)    #For Cramer's V
-insurance <- read.csv("C:\\Users\\admin\\Desktop\\SJC\\Projects\\R Proj\\Insurance\\insurance_claims.csv")
+library(dummies)      #For creating dummy variables
+library(ROCR)       #Library to plot ROC
+library(Metrics)    #Library to calculate AUC
+library(dplyr)
 
+#Importing the data 
+insurance <- read.csv("C:\\Users\\jyoth\\Desktop\\SEM 2\\Bhogle Sir\\R Proj\\Insurance\\Final\\auto_insurance_claims.csv")
+
+#****************************************************MISSING VALUES***************************************************
 #Get column names having '?' values
-null_cols = unique(names(insurance)[which(insurance == '?', arr.ind=T)[, "col"]])
+null_cols = unique(names(insurance)[which(insurance == '?', arr.ind=T)[, "col"]]);null_cols
 
 #Check the relevance of the columns with '?' values using:
 #1. Proportion of '?'
-sapply(insurance, function(x) length(x[x=="?"])/nrow(insurance))
-#2. Chi-sq Test
+sapply(insurance[,null_cols], function(x) length(x[x=="?"])/nrow(insurance))
+ #2. Chi-sq Test
 for (i in null_cols){
-  # print(i)
   coll = insurance[,i][which(insurance[,i] !='?', arr.ind=T)];coll
   fraud = insurance$fraud_reported[which(insurance[,i]!='?', arr.ind=T)]
-  #print("Chi-sq test results between Fraud cases reported and %s",%(i))
+  cat(sprintf("***Chi-sq test results between Fraudulent Cases Reported and %s***\n",i))
   print(chisq.test(coll,fraud))}
 
 #Since it's seen that these columns have no relevant effect on 'fraud_reported', drop them
-insurance[,c(null_cols,"policy_number")]<-NULL
+insurance[,c(null_cols,"policy_number", "incident_year", "insured_zip", "policy_bind_date","insured_zip",
+             "incident_date","incident_location", "incident_city", "policy_day", "policy_month", "policy_year")]<-NULL
 
-#PCA
+write.csv(insurance, "C:\\Users\\jyoth\\Desktop\\SEM 2\\Bhogle Sir\\R Proj\\Insurance\\Final\\cleaned_data.csv")
+
+#*************************************************NUMERICAL FEATURES*************************************************
+
+#MUlticollinearity
+num<-NULL
+num <- c(split(names(insurance),sapply(insurance, function(x) paste(class(x), collapse=" ")))$integer, split(names(insurance),sapply(insurance, function(x) paste(class(x), collapse=" ")))$numeric)
+num<-num[!(num %in% c("incident_hour_of_the_day", "auto_year"))]
+num
+corr1 <- cor(insurance[, num]);corr1     #Computing the correlation between the numerical variables
+write.csv(corr1, "C:\\Users\\jyoth\\Desktop\\SEM 2\\Bhogle Sir\\R Proj\\Insurance\\Final\\corr1.csv")
+
+
+#PCA, considering numerical variables with high correlation
 corr_nums<- c("months_as_customer","age","Avg_capital_loss","capital_gains","Avg_capital_gains","total_claim_amount","injury_claim","property_claim","vehicle_claim" )
 insurance.pca<-prcomp(insurance[corr_nums],center = TRUE, scale. = TRUE)
 summary(insurance.pca)
 insurance.pca
 var = insurance.pca$sdev^2
-prop_var <- var/sum(var); 
+prop_var <- var/sum(var)
+cumsum(prop_var)
 plot(cumsum(prop_var))  
 screeplot(insurance.pca, type="lines")
 biplot(insurance.pca,scale=0)
 insurance[, corr_nums] <- NULL
-insurance <- cbind(insurance, insurance.pca$x[,1:4])
-
-
-num<-NULL
-num <- c(split(names(insurance),sapply(insurance, function(x) paste(class(x), collapse=" ")))$integer, split(names(insurance),sapply(insurance, function(x) paste(class(x), collapse=" ")))$numeric)
-num<-num[!(num %in% c("insured_zip", "auto_year", "policy_number", "incident_hour_of_the_day",corr_nums))]
+#First 5 PCs explain 06% of the variance. Hence, only those were selected
+insurance <- cbind(insurance, insurance.pca$x[,1:5])
+names(insurance)
+#To double-check, if multicollinearity problem is fixed
+num<-num[!(num %in% c("insured_zip", "incident_hour_of_the_day", "auto_year",corr_nums))]
+num <- c(num, "PC1", "PC2", "PC3", "PC4", "PC5")
 num
-corr <- cor(insurance[, num])     #Computing the correlation between the numerical variables
-write.csv(corr, "corr.csv")
+corr2 <- cor(insurance[, num]);corr2     #Computing the correlation between the numerical variables
+write.csv(corr2, "C:\\Users\\jyoth\\Desktop\\SEM 2\\Bhogle Sir\\R Proj\\Insurance\\Final\\corr2.csv")
 
 #ANOVA to check the effect of numeric features on the target variable
 for (i in num){
-  blah <- aov(insurance[,i]~insurance$fraud_reported, data = insurance)
-  cat("--------------------------------------------------------")
+  anova_res <- aov(insurance[,i]~insurance$fraud_reported, data = insurance)
   cat(sprintf('\nAnova Table: fraud_reported and %s\n',i))
-  print(summary(blah))
+  print(summary(anova_res))
+  cat("----------------------------------------------------------------\n")
 }
-nums_final<-c("umbrella_limit","PC1")
-# names(insurance)
+nums_final<-c("umbrella_limit","PC1")  #Since only these variables had an effect on 'fraud_reported'
 
-rem<-c("policy_bind_date","insured_zip","incident_date","incident_location" )   #Other columns to be removed
-insurance[,rem]<-NULL
+#*************************************************CATEGORICAL FEATURES***********************************************
 
-others = names(insurance)[!(names(insurance) %in% c(num,rem,"fraud_reported"))]
+#Categorical features
+others = names(insurance)[!(names(insurance) %in% c(num,"fraud_reported"))]; others  
 
-categ<-dummy.data.frame(insurance,names=others)   #Creating dummy variables from the categorical variables 
-names(categ)      
-vars<-c()
-#Chi-Sq Test of Independence and Cramer's V computed for all the dummy vairables and the target variable
-for (i in names(categ)){
-  cat("\n------------------------------------------------------------------------------")
+#Chi-Sq Test of Independence and Cramer's V computed for all the dummy variables and the target variable
+cats <- c()
+for (i in others){
   cat(sprintf("\nChi-sq test results between \'fraud_reported\' and \"%s\" \n",i))
-  p <- chisq.test(categ[,i],insurance$fraud_reported)
+  p <- chisq.test(insurance[,i],insurance$fraud_reported)
   print(p)
   if(p$p.value<0.07){
-  vars<-c(vars,i)}
+    cats<-c(cats,i)}
   cat(sprintf("Cramer's V: %s\n", CramerV(insurance[,i],insurance$fraud_reported)))
-  }
-vars
-final<-c(vars,nums_final);final
+  cat("\n------------------------------------------------------------------------------\n")
+}
+cats     #Relevant categorical features 
 
-insurance[,!(names(insurance) %in% nums_final)]<-NULL
-insurance<- cbind(categ[,vars], insurance)     #Final cleaned dataset with the required features and the target variable
+cleaned_df <- insurance[,(names(insurance) %in% c(nums_final, "fraud_reported", cats))]
+names(cleaned_df)
+#Creating dummy variables
+categ<-dummy.data.frame(insurance,names=cats, sep=":", all=FALSE)   #Creating dummy variables from the categorical variables 
+names(categ)  
+# final<-c(names(categ), nums_final, 'fraud_reported');final       #Final variables
+
+insurance[,!(names(insurance) %in% c(nums_final, "fraud_reported"))]<-NULL  #Removing the other columns
+insurance<- cbind(categ, insurance)     #Final cleaned dataset with the required features and the target variable
+names(insurance)                        #Final variables - after preprocessing and EDA
+
+str(insurance)                          #Data type of the columns in a dataframe
+#Changing the data type of 'fraud_reported' into factor and labeling it as 0:No, 1:Yes
+insurance$fraud_reported <- factor(insurance$fraud_reported, labels = c(0,1))   
+dim(insurance)       #Shape: (1000,43)
+
+#****************************************************MODEL BUILDING**************************************************
+
 names(insurance)
-
 train<- insurance[1:(0.75*nrow(insurance)),]                  #Training data containing 75% of the entire dataset
-test<- insurance[(0.75*nrow(insurance)):nrow(insurance),]     #Training data containing 25% of the entire dataset
+test<- insurance[(0.75*nrow(insurance)):nrow(insurance),]     #Test data containing 25% of the entire dataset
+
+#Initial Logistic Regression Model with all the variables
+lr<-glm(fraud_reported~., data = train, family=binomial(link="logit"))   
+summary(lr)
+  
+prob <- predict(lr, test, type = "response")        
+pred <- ifelse(prob > 0.5, 1, 0)               #Predicted classes
+
+#Plot ROC Curve
+pr <- prediction(pred,test$fraud_reported)
+perf <- performance(pr, measure="tpr", x.measure="fpr")
+auc(test$fraud_reported,pred)     #0.8494395
+plot(perf) > auc(test$fraud_reported,pred)    #ROC Curve
 
 
-names(train)
-final<-c("insured_occupationexec-managerial","insured_hobbiescamping","insured_hobbieschess","insured_hobbiescross-fit","incident_severityMajor Damage","auto_year2004","umbrella_limit","fraud_reported")
-train<-train[,final]
-bm2<-glm(fraud_reported~., data = train, family=binomial(link="logit"))   #Logistic Model
-#bm1<-glm(fraud_reported~incident_severity_minor_damage+incident_severity_total_loss+incident_severity_trivial_damage+insured_hobbies_bungie.jumping+insured_hobbies_camping+insured_hobbies_chess+insured_hobbies_crossfit, data = train, family=binomial(link="logit"))
-summary(bm2)
+#Improving the model by selected only a few variables 
 
-probabilities <- predict(bm2,test, type = "response")        
-predicted.classes <- ifelse(probabilities > 0.5, "YES", "NO")
-predicted.classes
+final<-c("insured_hobbies:camping","insured_hobbies:chess","insured_hobbies:cross-fit","incident_severity:Major Damage","fraud_reported")
+train1<-train[,final]
+train1 =train1 %>%
+  rename('camping' = 'insured_hobbies:camping',
+         'chess' = 'insured_hobbies:chess',
+         'cross_fit'= "insured_hobbies:cross-fit",
+         'major_damage' = 'incident_severity:Major Damage')
+lr1<-glm(fraud_reported~., data = train1, family=binomial(link="logit"))  #Final Logistic Regression Model
+summary(lr1)
 
+test1 <- test[,final] 
+test1 =test1 %>%
+  rename('camping' = 'insured_hobbies:camping',
+         'chess' = 'insured_hobbies:chess',
+         'cross_fit'= "insured_hobbies:cross-fit",
+         'major_damage' = 'incident_severity:Major Damage')
+
+prob1 <- predict(lr1, test1, type = "response")        
+pred1 <- ifelse(prob1 > 0.5, 1, 0)               #Predicted classes
+
+#Plot ROC Curve
+pr1 <- prediction(pred1,test$fraud_reported)
+perf1 <- performance(pr1, measure="tpr", x.measure="fpr")
+auc(test$fraud_reported,pred1)     #0.9087746
+plot(perf1) > auc(test$fraud_reported,pred1)    #ROC Curve
+
+#Saving the model; Loaded in the UI 
+save(lr1, file="C:\\Users\\jyoth\\Desktop\\SEM 2\\Bhogle Sir\\R Proj\\Insurance\\Final\\LR_Model.rda")
+saveRDS(lr1, file = "C:\\Users\\jyoth\\Desktop\\SEM 2\\Bhogle Sir\\R Proj\\Insurance\\Final\\LR.rda")
